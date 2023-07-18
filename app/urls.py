c245ff1ca7cf5b2435651f9baa717e7d343ca1d8
santasly <mogotusylvia@gmail.com>
2023-07-18 13:45:04 +0300

    initial commit

--- app/urls.py
+++ app/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('',views.home,name="home"),
     path('insert',views.insert, name="insert"),
-    path('people',views.people,name='people')
+    path('people',views.people,name='people'),
+    path('delete/<id>',views.delete, name='delete'),
+    path('update/<id>',views.update,name='update')
 ]
--- app/views.py
+++ app/views.py
@@ -4,25 +4,39 @@ from django.http import HttpResponse
 from app.models import People
 
 
-
-#create your views here.
+# create your views here.
 def home(request):
     return render(request, 'home.html')
 
+
 def insert(request):
     if request.method == "POST":
-        name=request.POST.get('name')
-        school=request.POST.get('school')
-        email=request.POST.get('email')
+        name = request.POST.get('name')
+        school = request.POST.get('school')
+        email = request.POST.get('email')
 
-        person=People(name=name,school=school,email=email)
-        person.save() #saving in the db
+        person = People(name=name, school=school, email=email)
+        person.save()  # saving in the db
 
-      #  print(name,school,email)
+    #  print(name,school,email)
 
     return render(request, 'home.html')
 
+
 def people(request):
-    d=People.objects.all()
-    context={"data":d}
-    return render(request,'people.html',context) #we give people.html access to the data
+    d = People.objects.all()
+    context = {"data": d}
+    return render(request, 'people.html', context)  # we give people.html access to the data
+
+
+def delete(request, id):
+    dd = People.objects.get(id=id)
+    dd.delete()
+    return HttpResponse("delete successful")
+
+
+def update(request, id):
+    l = People.objects.get(id=id)
+    return render(request, "edit.html", {
+        "l": l
+    })
